eed2a8a03f8c3faef2d6422f4e9308e7656b6382
Ira Tka4eva <ira.tka4eva2011@yandex.ru>
2024-08-07 22:11:53 +0300

    Первое задание готово

--- 1.base-concepts/task.js
+++ 1.base-concepts/task.js
@@ -1,10 +1,27 @@
 "use strict"
 function solveEquation(a, b, c) {
+  let discriminant = b**2 - 4*a*c;
   let arr = [];
+
+  if (discriminant < 0) {
+    arr = [];
+  } else if (discriminant === 0) {
+    arr.push(-b / (2*a));
+  } else {
+    arr.push((-b + Math.sqrt(d)) / (2*a));
+    arr.push((-b - Math.sqrt(d) ) / (2*a));
+    
+  }  
   
   return arr;
 }
 
+
 function calculateTotalMortgage(percent, contribution, amount, countMonths) {
-  
+  let monthlyPercent = percent / 100 / 12;
+    let loanBody = amount - contribution;    
+    let monthlyPayment = loanBody * (monthlyPercent + monthlyPercent / (Math.pow(1 + monthlyPercent, countMonths) - 1));    
+    let totalAmount = monthlyPayment * countMonths;
+    
+    return parseFloat(totalAmount.toFixed(2));
 }
