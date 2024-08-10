d1391a474e740c2685f972edd9e59a5410942148
Ira Tka4eva <ira.tka4eva2011@yandex.ru>
2024-08-10 10:43:16 +0300

    Решение еще исправлено

--- 1.base-concepts/task.js
+++ 1.base-concepts/task.js
@@ -3,7 +3,7 @@ function solveEquation(a, b, c) {
   let d = b**2 - 4*a*c;
   let arr = [];
 
-  if (discriminant === 0) {
+  if (d === 0) {
     arr.push(-b / (2*a));
   } else if (d > 0) {
     arr.push((-b + Math.sqrt(d)) / (2*a));
