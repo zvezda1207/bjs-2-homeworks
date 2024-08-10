e8b3d8348f25446f3c000976b7f5f30b8314c41c
Ira Tka4eva <ira.tka4eva2011@yandex.ru>
2024-08-10 10:36:02 +0300

    Задание исправлено

--- 1.base-concepts/task.js
+++ 1.base-concepts/task.js
@@ -1,13 +1,11 @@
 "use strict"
 function solveEquation(a, b, c) {
-  let discriminant = b**2 - 4*a*c;
+  let d = b**2 - 4*a*c;
   let arr = [];
 
-  if (discriminant < 0) {
-    arr = [];
-  } else if (discriminant === 0) {
+  if (discriminant === 0) {
     arr.push(-b / (2*a));
-  } else {
+  } else if (d > 0) {
     arr.push((-b + Math.sqrt(d)) / (2*a));
     arr.push((-b - Math.sqrt(d) ) / (2*a));
   }  
