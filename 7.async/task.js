7d63381d394d2988879f8f4e0f08bc76662373c5
Ira Tka4eva <ira.tka4eva2011@yandex.ru>
2024-09-22 21:52:38 +0300

    Седьмое задание

---
+++ 7.async/task.js
@@ -0,0 +1,55 @@
+class AlarmClock {
+    constructor() {
+        this.alarmCollection = [];
+        this.intervalId = null;
+    }
+
+    addClock(time, callback) {
+        if (!time || !callback) {
+        throw new Error('Отсутствуют обязательные аргументы');
+        }
+        if (this.alarmCollection.some(alarm => alarm.time === time)) {
+            console.warn('Уже присутствует звонок на это же время');
+            return;
+        }
+        this.alarmCollection.push({callback, time, canCall: true});
+    }
+
+    removeClock(time) {
+        this.alarmCollection = this.alarmCollection.filter(alarm => alarm.time !== time);
+        }
+
+    getCurrentFormattedTime() {
+        const now = new Date();
+        return now.toTimeString().slice(0, 5);
+    }
+
+    start() {
+        if (this.intervalId !== null) {
+            return;
+        }
+        this.intervalId = setInterval(() => {
+            const currentTime = this.getCurrentFormattedTime();
+            this.alarmCollection.forEach(alarm => {
+                if (alarm.time === currentTime && alarm.canCall) {
+                    alarm.canCall = false;
+                    alarm.callback();  
+                }
+            });
+        }, 1000);
+    }
+
+    stop() {
+        clearInterval(this.intervalId);
+        this.intervalId = null;
+    }
+
+    resetAllCalls() {
+        this.alarmCollection.forEach(alarm => alarm.canCall = true);
+    }
+
+    clearAlarms() {
+        this.stop();
+        this.alarmCollection = [];
+    }
+}
